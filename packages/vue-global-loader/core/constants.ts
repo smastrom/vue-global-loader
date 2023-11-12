debda5ccecaf54ec9c49912328e04c2f273532e8
smastrom <smastrom@proton.me>
2023-11-12 10:25:06 +0100

    Core - Edit default options

--- packages/vue-global-loader/core/constants.ts
+++ packages/vue-global-loader/core/constants.ts
@@ -1,8 +1,8 @@
 import type { GlobalLoaderOptions } from './types'
 
 export const DEFAULT_OPTIONS: GlobalLoaderOptions = {
-   screenReaderMessage: 'Loading...',
-   transitionDuration: 3000,
+   screenReaderMessage: 'Loading',
+   transitionDuration: 300,
    foregroundColor: '#000',
    backgroundColor: '#fff',
    backgroundOpacity: 1,
